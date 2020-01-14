9c801f043b80c70ed381db2d7106ac4303975a15
Dr. Hussein Bakri <15527179+HusseinBakri@users.noreply.github.com>
2020-01-14 13:43:57 +0000

    Update ReadFromCSVorExcel.py

--- ReadFromCSVorExcel.py
+++ ReadFromCSVorExcel.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 import pandas as pd
 
-# Examples
+# Example code
+
 # Read from CSV via Pandas
 XYZ = pd.read_csv('XYZ.csv', index_col = 'Date')
-
 
 # To read back from Excel via Pandas
 XYY = pd.read_excel('XYZ.xlsx')
 print(XYY.head())
+
+# To read from Excel a specific sheet (like Sheet1)
+pd.read_excel('XYZ.xlsx',sheetname='Sheet1')
+
+# To Write to Excel a specific sheet (like Sheet 1) - NB: df is a Pandas dataframe
+df.to_excel('XYZ.xlsx',sheet_name='Sheet1')
+
+# To write to CSV (df being a dataframe)
+df.to_csv('Test.csv', index=False)
+
