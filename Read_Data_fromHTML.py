ac8497dbb06b9107b3097186c011535a81d887f3
Dr. Hussein Bakri <15527179+HusseinBakri@users.noreply.github.com>
2020-01-14 13:59:51 +0000

    Add files via upload

---
+++ Read_Data_fromHTML.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''
+Description:
+------------
+This Python script retrieves data from an HTML(web) page using Beautifulsoup and Pandas
+
+Requirements:
+------------
+Make sure that Pandas and BeautifulSoup4 are installed
+no need for anything else
+
+Usage: 
+-----
+python Read_Data_fromHTML.py
+
+Dr. Hussein Bakri
+Enjoy!
+'''
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+Web_URL = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
+Data_as_list = pd.read_html(Web_URL)
+Dataframe_fromList =  pd.DataFrame(Data_as_list[0]) 
+print(Dataframe_fromList)
